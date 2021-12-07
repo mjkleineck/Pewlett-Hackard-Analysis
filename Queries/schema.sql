022fe06d19f0c0b1db9470bb9257d2cf65cd36a4
Matthew Kleineck <mjkleineck@gmail.com>
2021-12-06 23:57:15 -0600

    updated queries and schema

--- Queries/schema.sql
+++ Queries/schema.sql
@@ -17,7 +17,7 @@ CREATE TABLE employees (
 );
 
 CREATE TABLE dept_manager (
-dept_no VARCHAR(4) NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
     emp_no INT NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
@@ -36,9 +36,9 @@ CREATE TABLE salaries (
 );
 
 CREATE TABLE dept_emp (
-dept_no VARCHAR(4) NOT NULL,
     emp_no INT NOT NULL,
-    from_date DATE NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
+	from_date DATE NOT NULL,
     to_date DATE NOT NULL,
 FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
@@ -50,8 +50,5 @@ CREATE TABLE titles (
 	title VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
-
-SELECT * FROM departments;
