921be86d1f53df883a6b77ad156914c5878ecfd4
Matthew Kleineck <mjkleineck@gmail.com>
2021-12-12 17:19:33 -0600

    corrected queries and csv exports

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -26,3 +26,23 @@ INTO retiring_titles
 FROM unique_titles
 GROUP BY title
 ORDER BY count DESC;
+
+-- Mentorship Eligibility table that holds the employees who are eligible
+-- to participate in a mentorship program
+SELECT DISTINCT ON (em.emp_no)
+	em.emp_no,
+	em.first_name,
+	em.last_name,
+	em.birth_date,
+	de.from_date,
+	de.to_date,
+	ti.title
+INTO mentorship_eligibilty
+FROM employees AS em
+INNER JOIN dept_emp AS de
+ON (em.emp_no = de.emp_no)
+INNER JOIN titles AS ti
+ON (em.emp_no = ti.emp_no)
+WHERE (de.to_date = '9999-01-01')
+	AND (em.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY em.emp_no;
